579c0608be5c4ebc793fc240e89c60d81ccd95ae
icondor2019 <ivalrxis14@gmail.com>
2025-09-15 19:06:08 -0500

    ADD: base notebook with mlflow model registry

--- app/configuration/settings.py
+++ app/configuration/settings.py
@@ -12,6 +12,9 @@ class Settings(BaseSettings):
     OPEN_API_PATH: str = "/swagger-ui"
     OPEN_API_JSON: str = "/openapi.json"
 
+    # MLFLOW setup
+    MLFLOW_TRACKING_URI: str = os.getenv('MLFLOW_TRACKING_URI', 'http://127.0.0.1:5000/')
+
     class Config():
         case_sensitive = True
 
